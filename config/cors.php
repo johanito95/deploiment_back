9fdc42a83091fb5ea96505f637442ef802894185
johanito95 <johansiaka@gmail.com>
2025-10-05 18:35:19 -0500

    modifier les cors pour autoriser les requetes qui viennent de vercel

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://deploiment-front-nmtjiehy8-johanito95s-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
